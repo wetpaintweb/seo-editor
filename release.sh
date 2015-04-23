03d7468f49deb61fa001891fa522ef08a572b044
Matt Harrison <matt@hallme.com>
2015-04-23 10:40:17 -0400

    update sed to be mac compatible switch git st to git status for clients that don't support st

--- release.sh
+++ release.sh
@@ -16,11 +16,11 @@ fi
 echo "Releasing version ${version}"
 
 echo "Setting version number in readme.txt and php files"
-sed -i'' -e "s/Stable tag: .*/Stable tag: ${version}/" readme.txt
-sed -i'' -e "s/Version:           .*/Version:           ${version}/" ${package}.php
-sed -i'' -e "s/this->version = '.*';/this->version = '${version}';/" includes/class-${package}.php
+sed -i '' -e "s/Stable tag: .*/Stable tag: ${version}/" readme.txt
+sed -i '' -e "s/Version:           .*/Version:           ${version}/" ${package}.php
+sed -i '' -e "s/this->version = '.*';/this->version = '${version}';/" includes/class-${package}.php
 
-if ([[ $(git st | grep readme.txt) ]] || [[ $(git st | grep ${package}.php) ]]); then
+if ([[ $(git status | grep readme.txt) ]] || [[ $(git status | grep ${package}.php) ]]); then
 	echo "Committing changes"
 	git add readme.txt
 	git add ${package}.php
