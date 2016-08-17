d650714bb29359a10dbb7f2a4ec1e589a68b56f6
Matt Harrison <matt@wetpaint.io>
2016-08-17 10:30:28 -0400

    update deploy readme to use the correct filename

--- deploy-readme.sh
+++ deploy-readme.sh
@@ -8,13 +8,13 @@ wordpressuser="wetpaintweb"
 package="seo-editor"
 
 echo "Pushing current README to WordPress.org"
-version=`grep "Stable tag:" README.txt | awk '{print $3}'`
+version=`grep "Stable tag:" readme.txt | awk '{print $3}'`
 
 echo "Checking out current trunk on Wordpress SVN"
 svn co https://plugins.svn.wordpress.org/${package}/trunk /tmp/readme-${package}
 
-echo "Copying in updated README.txt"
-cp "$(pwd)/README.txt" /tmp/readme-${package}/README.txt
+echo "Copying in updated readme.txt"
+cp "$(pwd)/readme.txt" /tmp/readme-${package}/readme.txt
 
 echo "Commiting new README to Wordpress SVN trunk"
 svn commit --username ${wordpressuser} -m"Updating README file" /tmp/readme-${package}
@@ -26,8 +26,8 @@ rm -rf /tmp/readme-${package}
 echo "Checking out current ${version} version on Wordpress SVN"
 svn co https://plugins.svn.wordpress.org/${package}/tags/${version} /tmp/readme-${package}
 
-echo "Copying in updated README.txt"
-cp "$(pwd)/README.txt" /tmp/readme-${package}/README.txt
+echo "Copying in updated readme.txt"
+cp "$(pwd)/readme.txt" /tmp/readme-${package}/readme.txt
 
 echo "Commiting new README to Wordpress SVN ${version} version"
 svn commit --username ${wordpressuser} -m"Updating README file" /tmp/readme-${package}
